39c7cd6b85ad37ee90ca1409eec1f1480dcd33e6
松下俊 <shunmatsu753@gmail.com>
2020-03-04 17:46:02 +0900

    検索ボックス位置修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,105 +1,20 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/main.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/utility.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/layout.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/icomoon/style.css') }}" rel="stylesheet">
-
-    @yield('css')
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark shadow" style="background: linear-gradient(90deg, rgba(7,0,117,1) 0%, rgba(13,0,255,1) 31%, rgba(2,179,215,1) 100%);">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img class="navbar-logo" src="{{ asset('images/capsule1.png') }}" style="padding:2px 0 2px 2px;">
-                    <span style="color:#fff">{{ config('app.name', 'Laravel') }}<span>
-                </a>
-                <form method="GET" action="{{ route('index') }}">
-                @csrf               
-                    <input type="text" name="keyword" value="{{ old('keyword') }}" class="rounded-lg border-info">
-                    <input type="submit" value="検索" class="rounded-lg border-info">
-                </form>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">ログイン</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">新規登録</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item">
-                                <a href="{{ route('create') }}" class='nav-link'>お薬を登録する</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        ログアウト
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="main" style="background:#fff;">
-        @if (session('flash_message'))
-            <div class="flash_message bg-success text-center py-3 my-0 mb30">
-                {{ session('flash_message') }}
-            </div>
-        @endif
-            @yield('content')
-        </main>
-        <footer class="footer p20" style="background: linear-gradient(90deg, rgba(7,0,117,1) 0%, rgba(13,0,255,1) 31%, rgba(2,179,215,1) 100%);">
-            @yield('footer')
-            <a href="{{ url('/') }}" style="text-decoration: none;"><small style="color:white;">おくすり手帳 2020</small></a>
-        </footer>
-    </div>
-</body>
-</html>
-
+@section('css')
+@endsection
+<!--@auth-->
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-xs-12 col-md-6 mt-3">
+        <div class="card" style="width: 18rem;">
+          <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect fill="#868e96" width="100%" height="100%"/><text fill="#dee2e6" dy=".3em" x="50%" y="50%">Image cap</text></svg>
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+    </div>    
+  </div>
+</div>
+<!--@endauth-->
+@section('footer')
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,11 +33,6 @@
                     <img class="navbar-logo" src="{{ asset('images/capsule1.png') }}" style="padding:2px 0 2px 2px;">
                     <span style="color:#fff">{{ config('app.name', 'Laravel') }}<span>
                 </a>
-                <form method="GET" action="{{ route('index') }}">
-                @csrf               
-                    <input type="text" name="keyword" value="{{ old('keyword') }}" class="rounded-lg border-info">
-                    <input type="submit" value="検索" class="rounded-lg border-info">
-                </form>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -45,7 +40,11 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <form method="GET" action="{{ route('index') }}">
+                            @csrf               
+                            <input type="text" name="keyword" value="{{ old('keyword') }}" class="rounded-lg border-info">
+                            <input type="submit" value="検索" class="rounded-lg border-info">
+                        </form>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
